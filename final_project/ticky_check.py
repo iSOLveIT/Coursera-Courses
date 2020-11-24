ba4c49e366e9e5e2a1c9c7e3cd7df602fd864157
Randy Duodu <duodurandy19@gmail.com>
2020-11-24 13:43:24 +0000

    Updated ticky_check.py

--- final_project/ticky_check.py
+++ final_project/ticky_check.py
@@ -1,63 +1,60 @@
 #!/usr/bin/env python3
-
-import sys
+''' The logfile to be processed has to be passed  to the script'''
+''' as an argument'''
 import re
-import os
-import operator
+import sys
 import csv
+import operator
 
+def statistics(logfile):
 
-error = {}
-per_user = {}
+  per_user = {}
+  error = {}
 
-dir_path = os.path.abspath('.')
-log_file_path = dir_path + '/' + sys.argv[1]
+  with open(logfile, "r") as file:
+    for line in file.readlines():
+      pattern = r": ([A-Z]*) ([\w ']*) [\[\]\d# ]*\(([\w\.]*)\)$"
+      message = re.search(pattern, line)
+      log_type = message.group(1)
+      log_message = message.group(2)
+      log_user = message.group(3)
+      #to create the user statistics
+      if log_type == "ERROR":
+        if log_message in error:
+          error[log_message] += 1
+        else:
+          error[log_message] = 1
+      #to create the user statistics
+      if log_user in per_user:
+        if log_type == "ERROR":
+          per_user[log_user][log_type] += 1
+        elif log_type == "INFO":
+          per_user[log_user][log_type] += 1
+      else:
+        if log_type == "ERROR":
+          per_user[log_user] = {"ERROR": 1, "INFO": 0}
+        elif log_type == "INFO":
+          per_user[log_user] = {"ERROR": 0, "INFO": 1}
 
-with open(log_file_path) as log_file:
-    data = log_file.readlines()
-    for datum in data:
-        line = datum.strip()
-        if "INFO" in line:
-            pattern = r"ticky: INFO: ([\w ]*) (\[#[\d]+\]) (\([\w]+\))"
-            result = re.search(pattern, line)
-            if result is None:
-                continue
-            content = result.groups()
-            username = content[2].strip('()')
+#Here, the dictionaries will be sorted
+  per_user = sorted(per_user.items())
+  error = sorted(error.items(), key = operator.itemgetter(1), reverse = True)
+  return per_user, error
 
-            user_data = per_user.get(username, {"INFO": 0, "ERROR": 0})
-            user_data['INFO'] = user_data.get('INFO', 0) + 1
-            per_user[username] = user_data
+def to_csv(per_user, error):
+  with open("user_statistics.csv", "w") as users_csv:
+    writer = csv.writer(users_csv)
+    writer.writerow(["Username", "INFO", "ERROR"])
+    for item in per_user:
+      user, log_type = item
+      line = [user,log_type["INFO"],log_type["ERROR"]]
+      writer.writerow(line)
+  with open("error_message.csv", "w") as error_csv:
+    writer = csv.writer(error_csv)
+    writer.writerow(["Error","Count"])
+    writer.writerows(error)
 
-        elif "ERROR" in line:
-            pattern = r"ticky: ERROR: ([\w ]*) (\([\w]+\))"
-            result = re.search(pattern, line)
-            if result is None:
-                continue
-            content = result.groups()
-            username = content[1].strip('()')
-            error_key = content[0]
-            error[error_key] = error.get(error_key, 0) + 1
-
-            user_data = per_user.get(username, {"INFO": 0, "ERROR": 0})
-            user_data['ERROR'] = user_data.get('ERROR', 0) + 1
-            per_user[username] = user_data
-
-        continue
-
-sorted_error = sorted(error.items(), key=operator.itemgetter(1), reverse=True)
-sorted_error.insert(0, ("Error", "Count"))
-sorted_per_user = [(i[0], i[1].get("INFO"), i[1].get("ERROR")) for i in sorted(per_user.items())]
-sorted_per_user.insert(0,  ("Username", "INFO", "ERROR"))
-
-
-def write_csv(sorted_dictionary, csv_file_name):
-    csv_file_path = dir_path + '/'+ csv_file_name
-    with open(csv_file_path, 'w') as file:
-        csv_writer = csv.writer(file)
-        csv_writer.writerows(sorted_dictionary)
-
-
-write_csv(sorted_error, 'error_message.csv')
-write_csv(sorted_per_user, 'user_statistics.csv')
-
+if __name__ == "__main__":
+  logfile = sys.argv[1]
+  per_user, error = statistics(logfile)
+  to_csv(per_user,error)
